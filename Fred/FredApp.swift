ba9942171e6c060cab4d9b03e04791031e6306ae
devin12422 <kramerdevin321@gmail.com>
2023-02-27 12:54:24 -0500

    Initial Commit

---
+++ Fred/FredApp.swift
@@ -0,0 +1,17 @@
+//
+//  FredApp.swift
+//  Fred
+//
+//  Created by Devin Kramer (student LM) on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
